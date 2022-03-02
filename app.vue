167bf89718ababaa26ac6584ca7c51c7ddb481a2
MateBook <examletru@gmail.com>
2022-03-03 01:15:56 +0300

    first commit

--- app.vue
+++ app.vue
@@ -1,116 +1,19 @@
 <script lang="ts" setup>
-import bridge from '@vkontakte/vk-bridge';
+import bridge from "@vkontakte/vk-bridge";
 
-// Отправляет событие нативному клиенту на инициализацию приложения
 bridge.send("VKWebAppInit", {});
-bridge
-  .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
-  .then(data => {
-    bridge
-      .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
-      .then(data => {
-        bridge
-          .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
-          .then(data => {
-            bridge
-              .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
-              .then(data => {
-                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
-              })
-              .catch(error => {
-                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
-              });
-          })
-          .catch(error => {
-            bridge
-              .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
-              .then(data => {
-                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
-              })
-              .catch(error => {
-                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
-              });
-          });
-      })
-      .catch(error => {
-        bridge
-          .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
-          .then(data => {
-            bridge
-              .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
-              .then(data => {
-                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
-              })
-              .catch(error => {
-                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
-              });
-          })
-          .catch(error => {
-            bridge
-              .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
-              .then(data => {
-                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
-              })
-              .catch(error => {
-                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
-              });
-          });
-      });
-  })
-  .catch(error => {
-    bridge
-      .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
-      .then(data => {
-        bridge
-          .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
-          .then(data => {
-            bridge
-              .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
-              .then(data => {
-                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
-              })
-              .catch(error => {
-                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
-              });
-          })
-          .catch(error => {
-            bridge
-              .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
-              .then(data => {
-                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
-              })
-              .catch(error => {
-                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
-              });
-          });
-      })
-      .catch(error => {
-        bridge
-          .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
-          .then(data => {
-            bridge
-              .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
-              .then(data => {
-                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
-              })
-              .catch(error => {
-                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
-              });
-          })
-          .catch(error => {
-            bridge
-              .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
-              .then(data => {
-                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
-              })
-              .catch(error => {
-                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
-              });
-          });
-      });
+bridge.subscribe((e) => console.log(e));
+
+const allowMessages = () => {
+  bridge.send("VKWebAppAllowMessagesFromGroup", {
+    group_id: 170533771,
+    key: "dBuBKe1kFcdemzB",
   });
+};
 </script>
 
 <template>
-  <div>Полезные материалы по информатике ЕГЭ</div>
+  <div>
+    <button @click="allowMessages">Подписаться</button>
+  </div>
 </template>

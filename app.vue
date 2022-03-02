a467392dbe8646ab5e956fb84e846d087e146619
MateBook <examletru@gmail.com>
2022-03-02 19:21:35 +0300

    first commit

--- app.vue
+++ app.vue
@@ -3,11 +3,114 @@ import bridge from '@vkontakte/vk-bridge';
 
 // Отправляет событие нативному клиенту на инициализацию приложения
 bridge.send("VKWebAppInit", {});
-bridge.send("VKWebAppAllowMessagesFromGroup", {"group_id": 170533771, "key": "dBuBKe1kFcdemzB"});
+bridge
+  .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
+  .then(data => {
+    bridge
+      .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
+      .then(data => {
+        bridge
+          .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
+          .then(data => {
+            bridge
+              .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
+              .then(data => {
+                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
+              })
+              .catch(error => {
+                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
+              });
+          })
+          .catch(error => {
+            bridge
+              .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
+              .then(data => {
+                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
+              })
+              .catch(error => {
+                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
+              });
+          });
+      })
+      .catch(error => {
+        bridge
+          .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
+          .then(data => {
+            bridge
+              .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
+              .then(data => {
+                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
+              })
+              .catch(error => {
+                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
+              });
+          })
+          .catch(error => {
+            bridge
+              .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
+              .then(data => {
+                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
+              })
+              .catch(error => {
+                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
+              });
+          });
+      });
+  })
+  .catch(error => {
+    bridge
+      .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
+      .then(data => {
+        bridge
+          .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
+          .then(data => {
+            bridge
+              .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
+              .then(data => {
+                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
+              })
+              .catch(error => {
+                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
+              });
+          })
+          .catch(error => {
+            bridge
+              .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
+              .then(data => {
+                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
+              })
+              .catch(error => {
+                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
+              });
+          });
+      })
+      .catch(error => {
+        bridge
+          .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
+          .then(data => {
+            bridge
+              .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
+              .then(data => {
+                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
+              })
+              .catch(error => {
+                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
+              });
+          })
+          .catch(error => {
+            bridge
+              .send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" })
+              .then(data => {
+                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
+              })
+              .catch(error => {
+                bridge.send("VKWebAppAllowMessagesFromGroup", { "group_id": 170533771, "key": "dBuBKe1kFcdemzB" });
+              });
+          });
+      });
+  });
 </script>
 
 <template>
-  <div>
-    Шапка Заголовок Описание КнопкаПодписаться
-  </div>
+  <div>Полезные материалы по информатике ЕГЭ</div>
 </template>

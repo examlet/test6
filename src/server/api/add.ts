7dd51e76ad715c6f7d692c9fa2afbb2768745b2d
MateBook <examletru@gmail.com>
2022-03-03 10:17:53 +0300

    first commit

--- src/server/api/add.ts
+++ src/server/api/add.ts
@@ -5,7 +5,7 @@ export default async (req, res) => {
 
     let formData = new URLSearchParams();
     formData.append("vk_group_id", "170533771");
-    formData.append("access_token", process.env.SECRET);
+    formData.append("access_token", "c0c3c9714a05f7f66527146f826dd2ce1c49b33bb9269923");
     formData.append("v", "2");
     formData.append("subscription_id", "1584736");
     formData.append("vk_user_id", `${query.userVkId}`);
